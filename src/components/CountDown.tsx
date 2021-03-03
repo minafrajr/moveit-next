215c1da437a0d49f6605b15d18623f3e1d8cf37e
minafrajr <carlos.minafrajr@gmail.com>
2021-03-03 09:57:35 -0300

    finish countdown

--- src/components/CountDown.tsx
+++ src/components/CountDown.tsx
@@ -1,13 +1,26 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import styles from "../styles/components/CountDown.module.css";
 
 export function CountDown() {
   const [time, setTime] = useState(25 * 60);
+  const [active, setActive] = useState(false);
+
   const minutes = Math.floor(time / 60);
   const seconds = time % 60;
-
   const [minuteLeft, minuteRight] = String(minutes).padStart(2, "0").split("");
   const [secondLeft, secondRight] = String(seconds).padStart(2, "0").split("");
+  
+  function startCountDown() {
+    setActive(true);
+  }
+
+  useEffect(() => {
+    if (active && time > 0) {
+      setTimeout(() => {
+        setTime(time - 1);
+      }, 1000);
+    }
+  }, [active, time])
 
   return (
     <div>
@@ -18,11 +31,11 @@ export function CountDown() {
         </div>
         <span>:</span>
         <div>
-          <span>{secondLeft</span>
+          <span>{secondLeft}</span>
           <span>{secondRight}</span>
         </div>
       </div>
-      <button type="button" className={styles.countDownButton}>
+      <button type="button" className={styles.countDownButton} onClick={startCountDown}>
         Iniciar um ciclo
       </button>
     </div>
